675eba69ba35fc4e6b7f12be15900ba938dcbdc1
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2022-12-12 23:59:24 +0100

    schedule

--- scheduler/src/components/Schedule.tsx
+++ scheduler/src/components/Schedule.tsx
@@ -1,32 +1,61 @@
 import { useState } from "react"
 
+
 const Schedule = () => {
 
-const [numberOfClickedFields, setnumberOfClickedFields]= useState(0)
+const [numberOfNotes, setNumberOfNotes]= useState (0)
+
+    
+
+    const displayMessage = (e:any) => {
+        let clickedField = e.target.parentNode
+        let newText:string = e.target.value
+        // post phrase to parent element
+        let newPhrase = document.createElement("p")
+        let newId = "note" + numberOfNotes
+        newPhrase.id = newId;
+        setNumberOfNotes(numberOfNotes+1)
+        newPhrase.textContent = newText;
+        // add remove note button
+        let newRemoveButton = document.createElement("div")
+        newRemoveButton.innerHTML = "O"
+        newRemoveButton.onclick = () => {
+            let noteToDelete:any = document.getElementById(newId)
+            let parentElement = noteToDelete.parentNode;
+            parentElement?.removeChild(noteToDelete)
+        }
+        // append childs to parents
+        newPhrase.appendChild(newRemoveButton)
+        clickedField.appendChild(newPhrase)
+        // remove inputfield
+        clickedField.removeChild(e.target)
+    }
+
+    const checkPressedKey = (e:any) =>{
+    if (e.keyCode == 13){
+        e.target.onblur = ""
+        displayMessage(e)
+    }
+    }
 
     const editField = (e:any) =>{
         var clickedField = e.target
-    
-        if(e.target.nodeName == "TD"){
-          
-        let newId:string =  "clickedField" + numberOfClickedFields
-        setnumberOfClickedFields(numberOfClickedFields+1)
-        clickedField.id += newId
-       
-        let newUserInput = document.createElement("input");
-        newUserInput.className += "userNote"
+        if(clickedField.nodeName == "TD"){ // check if clicked field was an TD 
+        let newElement = document.createElement("input");
+        newElement.onkeyup = checkPressedKey
+        newElement.placeholder = "Enter your note friend"
+        newElement.onblur = displayMessage // onblur event to leave a node when input has finished
+        clickedField.appendChild(newElement)
+        
+    }
+    }
 
-        let myElement = document.getElementById(newId)
-        console.log(myElement)
-            myElement?.appendChild(newUserInput)
-     
-    }
-    }
 
 return(<div id="schedule" onClick={editField}>
+
     <table>
         <thead> 
-        
+            <tr>
         <th>Monday</th>
         <th>Tuesday</th>
         <th>Wednesday</th>
@@ -34,11 +63,11 @@ return(<div id="schedule" onClick={editField}>
         <th>Friday</th>
         <th>Saturday</th>
         <th>Sunday</th>
+        </tr>
     </thead>
   
-    
+    <tbody>
     <tr>
-        
         <td></td>
         <td></td>
         <td></td>
@@ -77,7 +106,7 @@ return(<div id="schedule" onClick={editField}>
         <td></td>
         <td></td>
     </tr>
-     
+    </tbody>
     </table>
 </div>)
 }
